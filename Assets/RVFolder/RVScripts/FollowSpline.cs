33d47224c67a0bee419fc74525350b283d57a4b6
MochiiMochi <link41578@hotmail.com>
2026-03-25 19:30:24 -0400

    Push

--- Assets/RVFolder/RVScripts/FollowSpline.cs
+++ Assets/RVFolder/RVScripts/FollowSpline.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class FollowSpline : MonoBehaviour
 {
+    private SplineContainer _currentFollowingSpline;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,12 @@ public class FollowSpline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Spline inside Current Following: " + _currentFollowingSpline);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var _getSpline = other.GetComponent<SplineTriggerZone>();
+        _currentFollowingSpline = _getSpline.SplineInTrigger();
     }
 }
