e5a8a01004039a6d819d736dc2b6b1b9d68be413
aleenagraveline <124313468+aleenagraveline@users.noreply.github.com>
2026-03-26 00:57:54 -0400

    start screen linked with join screen & finished

--- Assets/Scripts/PlayerSelection.cs
+++ Assets/Scripts/PlayerSelection.cs
@@ -4,16 +4,22 @@ using System.Collections;
 
 public class PlayerSelection : MonoBehaviour
 {
+    [SerializeField] private GameObject[] playerIndicators;
+    [SerializeField] private GameObject[] joinScreens;
+    [SerializeField] float delay;
+
+    private MPManager mp;
     private InputAction playerSelectAction;
     private InputAction confirmAction;
-    [SerializeField] float delay;
     private bool delayComplete;
-    private int numPlayers;
-    [SerializeField] private GameObject[] playerIndicators;
+    private int numPlayers = 1;
+    private GameObject currIndicator;
+    private GameObject joinScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        mp = FindFirstObjectByType<MPManager>();
         playerSelectAction = InputSystem.actions.FindAction("PlayerSelect");
         confirmAction = InputSystem.actions.FindAction("Confirm");
         delayComplete = true;
@@ -31,23 +37,57 @@ public class PlayerSelection : MonoBehaviour
             numPlayers = Mathf.Clamp(numPlayers, 1, 4);
             if (direction.x > 0 && numPlayers > 1)
             {
-                playerIndicators[numPlayers - 1].SetActive(true);
+                currIndicator = playerIndicators[numPlayers - 1];
+                currIndicator.SetActive(true);
+                currIndicator.transform.GetChild(0).gameObject.SetActive(true); // get the text object that shows the numPlayers selected and set it to active
+                playerIndicators[numPlayers - 2].transform.GetChild(0).gameObject.SetActive(false); // set the previously active text object to inactive
             }
             else
             {
-                playerIndicators[numPlayers].SetActive(false);
+                currIndicator = playerIndicators[numPlayers];
+                currIndicator.SetActive(false);
+                playerIndicators[numPlayers - 1].transform.GetChild(0).gameObject.SetActive(true); // get the text object that shows the numPlayers selected and set it to active
             }
+
             StartCoroutine(JoystickCooldown());
         }
 
-        Debug.Log("NumPlayers: " + numPlayers);
+        if(confirmAction.WasPressedThisFrame())
+        {
+            GoToJoinScreen();
+        }
 
+        //Debug.Log("NumPlayers: " + numPlayers);
     }
 
-    IEnumerator JoystickCooldown()
+    private IEnumerator JoystickCooldown()
     {
         delayComplete = false;
         yield return new WaitForSeconds(delay);
         delayComplete = true;
+    }
+
+    private void GoToJoinScreen()
+    {
+        switch(numPlayers)
+        {
+            case 1:
+                joinScreen = Instantiate(joinScreens[0]);
+                break;
+            case 2:
+                joinScreen = Instantiate(joinScreens[1]);
+                break;
+            case 3:
+                joinScreen = Instantiate(joinScreens[2]);
+                break;
+            case 4:
+                joinScreen = Instantiate(joinScreens[3]);
+                break;     
+        }
+
+        joinScreen.GetComponent<JoinScreenManager>().SetNumPlayers(numPlayers);
+        mp.SetJoinScreen();
+
+        Destroy(gameObject.transform.parent.gameObject);
     }
 }
