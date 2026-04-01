1cba26f61ee4ac126c105ebfa0b67c2e14d4d294
MochiiMochi <link41578@hotmail.com>
2026-04-01 19:08:17 -0400

    Spline durations are now independently linked to their unique splines

--- Assets/RVFolder/RVScripts/FollowSpline.cs
+++ Assets/RVFolder/RVScripts/FollowSpline.cs
@@ -7,18 +7,26 @@ using UnityEngine.Splines;
 public class FollowSpline : MonoBehaviour
 {
     // Components
+    [SerializeField] private CarControl _kartBoost;
     [SerializeField] private SplineContainer _currentFollowingSpline;
     [SerializeField] private InputActionReference _cancelAction;
 
     // Variables
     [SerializeField] private float _speed = 0.01f;
-    [SerializeField] private float _duration = 3f;
+    [SerializeField] private float _rubbleBoostDuration = 1f;
+    private float _duration = 3f;
     private float _time;
+
+    private void Awake()
+    {
+        _kartBoost = this.GetComponent<CarControl>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         var _getSpline = other.GetComponent<SplineTriggerZone>();
         _currentFollowingSpline = _getSpline.SplineInTrigger();
+        _duration = _getSpline.TriggerZoneDuration();
         AnimateAlongPathEngage();
     }
 
@@ -43,8 +51,10 @@ public class FollowSpline : MonoBehaviour
             float3 tangent = _currentFollowingSpline.EvaluateTangent(_time);
             transform.rotation = Quaternion.LookRotation(tangent);
 
+            // If accelerate is hit after a couple seconds into the duration, players can immediately hop off of it
             if (_time >= .5f && _cancelAction.action.triggered)
             {
+                _kartBoost.RubbleBoost(_rubbleBoostDuration);
                 yield break;
             }
 
--- Assets/RVFolder/RVScripts/SplineTriggerZone.cs
+++ Assets/RVFolder/RVScripts/SplineTriggerZone.cs
@@ -4,6 +4,7 @@ using UnityEngine.Splines;
 public class SplineTriggerZone : MonoBehaviour
 {
     [SerializeField] private SplineContainer _splineToFollow;
+    [SerializeField] private float _duration;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +15,10 @@ public class SplineTriggerZone : MonoBehaviour
     public SplineContainer SplineInTrigger()
     {
         return _splineToFollow;
+    }
+
+    public float TriggerZoneDuration()
+    {
+        return _duration;
     }
 }
