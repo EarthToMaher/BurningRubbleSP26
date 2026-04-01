bd64520937f30a9aba462fd07262d7bbd31bbaaa
MochiiMochi <link41578@hotmail.com>
2026-04-01 18:44:37 -0400

    Implemented Dynamic Spline interaction
    - A spline can be created and be applied a trigger zone script where when a player passes it, they'll automatically follow along the spline
    - The spline's duration can be adjusted on the Player script

--- Assets/RVFolder/RVScripts/FollowSpline.cs
+++ Assets/RVFolder/RVScripts/FollowSpline.cs
@@ -1,26 +1,47 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
 
 public class FollowSpline : MonoBehaviour
 {
+    // Components
     [SerializeField] private SplineContainer _currentFollowingSpline;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Variables
+    [SerializeField] private float _speed = 0.01f;
+    [SerializeField] private float _duration = 3f;
+    private float _time;
 
     private void OnTriggerEnter(Collider other)
     {
         var _getSpline = other.GetComponent<SplineTriggerZone>();
         _currentFollowingSpline = _getSpline.SplineInTrigger();
+        AnimateAlongPathEngage();
     }
 
+    public void AnimateAlongPathEngage()
+    {
+        StartCoroutine(AnimateAlongPath());
+    }
 
+    IEnumerator AnimateAlongPath()
+    {
+        _time = 0f; // Reset timer
+
+        while (_time < 1f)
+        {
+            _time += Time.deltaTime / _duration;
+
+            // Evaluates the Splines position relative to the time, then moves the player along that spline (time)
+            float3 position = _currentFollowingSpline.EvaluatePosition(_time);
+            transform.position = position;
+
+            // Properly aligns the player along the splines curve
+            float3 tangent = _currentFollowingSpline.EvaluateTangent(_time);
+            transform.rotation = Quaternion.LookRotation(tangent);
+
+            yield return null;
+        }
+    }
 }
